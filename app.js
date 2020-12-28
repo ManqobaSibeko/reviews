f5c237b084562a8a9701294085ee454cb5ebce58
unknown <manqsibeko@gmail.com>
2020-12-28 19:55:05 -0200

    adding starting project

---
+++ app.js
@@ -0,0 +1,17 @@
+const review = [{
+    
+    id:1,
+    name: "Susan Smith",
+    job:"Web develop",
+    img:"https://www.pexels.com/photo/stylish-woman-sitting-on-floor-in-light-studio-5878811/",
+    text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque dicta quia quaerat provident amet quibusdam nemo vero ullam nobis minus fuga laborum, ab, illum deleniti possimus tempore saepe iure nisi! Dicta quisquam culpa ullam repudiandae corrupti repellat sit atque nihil temporibus quasi officiis velit aperiam fugiat dolores assumenda aliquam, praesentium impedit maiores molestias",
+},
+{
+    
+    id:2,
+    name: "John Mike",
+    job:"Web develop",
+    img:"https://www.pexels.com/photo/woman-in-white-shirt-looking-out-the-window-4786952/",
+    text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque dicta quia quaerat provident amet quibusdam nemo vero ullam nobis minus fuga laborum, ab, illum deleniti possimus tempore saepe iure nisi! Dicta quisquam culpa ullam repudiandae corrupti repellat sit atque nihil temporibus quasi officiis velit aperiam fugiat dolores assumenda aliquam, praesentium impedit maiores molestias",
+},
+]
